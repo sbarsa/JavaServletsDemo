91abf843367ab471b21c8c65832c767984c2f5dc
Sergiu Barsa <sergiubarsa@gmail.com>
2025-10-26 12:56:04 -0600

    feat(*): add user model and service

--- src/main/java/com/sergiubarsa/myfancypdfinvoices/context/Application.java
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/context/Application.java
@@ -2,9 +2,11 @@ package com.sergiubarsa.myfancypdfinvoices.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sergiubarsa.myfancypdfinvoices.service.InvoiceService;
+import com.sergiubarsa.myfancypdfinvoices.service.UserService;
 
 public class Application {
 
     public static final ObjectMapper objectMapper = new ObjectMapper();
     public static final InvoiceService invoiceService = new InvoiceService();
+    public static final UserService userService = new UserService();
 }
--- src/main/java/com/sergiubarsa/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/sergiubarsa/myfancypdfinvoices/service/InvoiceService.java
@@ -1,6 +1,8 @@
 package com.sergiubarsa.myfancypdfinvoices.service;
 
+import com.sergiubarsa.myfancypdfinvoices.context.Application;
 import com.sergiubarsa.myfancypdfinvoices.model.Invoice;
+import com.sergiubarsa.myfancypdfinvoices.model.User;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,6 +13,11 @@ public class InvoiceService {
     private final List<Invoice> invoices = new CopyOnWriteArrayList<>();
 
     public Invoice create(String userId, int amount) {
+        User user = Application.userService.findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
+
         Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
         invoices.add(invoice);
 
